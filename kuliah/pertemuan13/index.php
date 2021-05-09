990ad6bf139b8cd5a7b977b3a1f5c76115faddc9
Nurul Mustofa <nioke8090@gmail.com>
2021-05-09 10:14:05 +0700

    registrasi dan persiapan membuat ajax

---
+++ kuliah/pertemuan13/index.php
@@ -0,0 +1,82 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+  # code...
+  header("Location: login.php");
+  exit;
+}
+
+require 'functions.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+// ketika tombol cari di klik
+if (isset($_POST['cari'])) {
+  # code...
+  $mahasiswa = cari($_POST['keyword']);
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"> -->
+</head>
+
+<body>
+  <a href="logout.php">logout</a>
+  <h2>Daftar Mahasiswa</h2>
+
+  <a href="tambah.php">Tambah Data Mahasiswa</a>
+  <br><br>
+
+  <form action="" method="POST">
+    <input type="text" name="keyword" size="40" placeholder="Masukkan Keywords Pencarian..." autocomplete="off" autofocus class="keyword">
+    <button type="submit" name="cari" class="tombol-cari">Cari</button>
+  </form>
+  <br>
+
+  <div class="container">
+    <table border="1" cellpadding="10" cellspacing="0">
+      <tr>
+        <th>#</th>
+        <th>Gambar</th>
+        <th>Nama</th>
+        <th>Aksi</th>
+      </tr>
+
+      <?php if (empty($mahasiswa)) : ?>
+        <tr>
+          <td colspan="4">
+            <p style="color: red; font-style: italic;">data Mahasiswa tidak ditemukan!</p>
+          </td>
+        </tr>
+      <?php endif; ?>
+
+
+
+      <?php $i = 1;
+      foreach ($mahasiswa as $m) : ?>
+
+        <tr>
+          <td><?= $i++; ?></td>
+          <td><img src="img/<?= $m['gambar']; ?>" alt="" style="width: 100px;" height="100px"></td>
+          <td><?= $m['nama']; ?></td>
+          <td>
+            <a href="detail.php?id=<?= $m['id']; ?>">lihat detail</a>
+          </td>
+        </tr>
+
+      <?php endforeach; ?>
+    </table>
+  </div>
+
+  <script src="js/script.js"></script>
+</body>
+
+</html>
